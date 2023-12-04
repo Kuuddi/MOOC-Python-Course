49e87ff05a5839b1442f60088d277c6885e2de69
kuuttikeltto <ytkuuddi@gmail.com>
2023-12-04 11:15:32 +0200

    Tehtäviä

--- Osa 2/Lisää ehtolauseita/Vanhempi.py
+++ Osa 2/Lisää ehtolauseita/Vanhempi.py
@@ -5,10 +5,10 @@ person2 = input("Nimi: ")
 age2 = int(input("Ikä: "))
 
 if age1 == age2:
-    print(f"Vanhempi on {person1}")
+    print(f"{person1} ja {person2} ovat yhtä vanhoja")
 
-elif age1 > age2:
+elif age2 > age1:
     print(f"Vanhempi on {person2}")
 
-elif age1 == age2:
-    print(f"{person1} ja {person2} ovat yhtä vanhoja")
+elif age1 > age2:
+    print(f"Vanhempi on {person1}")
