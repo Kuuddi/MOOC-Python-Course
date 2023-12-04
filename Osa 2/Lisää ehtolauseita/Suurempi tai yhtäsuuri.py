faca2c65830898fb115ceb67aa85a61ea5146238
kuuttikeltto <ytkuuddi@gmail.com>
2023-12-04 10:51:19 +0200

    Tehtäviä

---
+++ Osa 2/Lisää ehtolauseita/Suurempi tai yhtäsuuri.py
@@ -0,0 +1,11 @@
+num1 = int(input("Anna ensimmäinen luku: "))
+num2 = int(input("Anna toinen luku: "))
+
+if num1 == num2:
+    print("Luvut ovat yhtä suuret!")
+
+elif num1 > num2:
+    print(f"Suurempi luku: {num1}")
+
+elif num2 > num1:
+    print(f"Suurempi luku: {num2}")
---
+++ Osa 2/Lisää ehtolauseita/Täysi-ikäisyys.py
@@ -0,0 +1,7 @@
+age = int(input("Kuinka vanha olet? "))
+
+if age >= 18:
+    print("Olet täysi-ikäinen!")
+
+else:
+    print("Et ole täysi-ikäinen!")
