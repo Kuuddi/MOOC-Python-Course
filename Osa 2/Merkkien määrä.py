ffb8e5a5b88fa638f6f410a5523aa3ce11d0102d
kuuttikeltto <ytkuuddi@gmail.com>
2023-12-04 10:36:26 +0200

    Directory structure

--- Osa 2/Merkkien määrä.py
+++
@@ -1,6 +0,0 @@
-word = input("Anna sana: ")
-
-if len(word) > 1:
-    print(f"Sanassa {word} on {len(word)} kirjainta")
-
-print("Kiitos!")
--- Osa 2/Tyyppimuunnos.py
+++
@@ -1,7 +0,0 @@
-number = float(input("Anna lämpötila: "))
-
-whole = int(number)
-decimal = number - whole
-
-print(f"Kokonaisosa: {whole}")
-print(f"Desimaaliosa: {decimal}")
